fff5ad3f866657cd0e33df36e4bf0ac7968f8e77
a1xg <64093496+a1xg@users.noreply.github.com>
2021-11-07 19:37:55 +0200

    Refactor

--- backend/news/settings.py
+++ backend/news/settings.py
@@ -31,18 +31,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'backend',
-    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- backend/news/urls.py
+++ backend/news/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
-    path('', include('frontend.urls')),
+    path('auth', include('rest_framework.urls')),
+    path('api/v1/', include('backend.urls')),
 ]
