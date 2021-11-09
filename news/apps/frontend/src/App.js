a62b14a5142d3b34b99ed8504cfe00a31b967186
a1xg <64093496+a1xg@users.noreply.github.com>
2021-11-09 20:46:21 +0200

    Delete frontend auth, redux, etc

--- news/apps/frontend/src/App.js
+++ news/apps/frontend/src/App.js
@@ -2,32 +2,32 @@ import React from 'react';
 import { BrowserRouter, Route, NavLink } from "react-router-dom";
 import NewsList from './components/News/NewsList/NewsList.jsx';
 import NewsDetail from './components/News/NewsDetail/NewsDetail.jsx';
-import Login from './components/Authorization/Login.js';
-import SignUp from './components/Authorization/SignUp.js';
-import { Provider } from 'react-redux';
-import store from './store.js'
+
 // TODO сделать формы авторизации, регистрации и кнопку для выхода на фронте
 
 
 const App = (props) => {
-  console.log('app')
+  console.log('app props', props)
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div className="app">
-          <p>
-            <NavLink to='/login'>Login </NavLink>|
-            <NavLink to='/signup'> Signup</NavLink>
-          </p>
-          <div>
-            <Route exact path='/' component={NewsList} />
-            <Route exact path='/news/detail/:postID' component={NewsDetail} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={SignUp} />
-          </div>
+    <BrowserRouter>
+      <div className="app">
+        <p>
+          {/*
+           <NavLink to='/login'>Login </NavLink>|
+          <NavLink to='/signup'> Signup</NavLink>
+          */}
+        </p>
+        <div>
+          <Route exact path='/' component={NewsList} />
+          <Route exact path='/news/detail/:postID' component={NewsDetail} />
+          {/*
+           <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          */}
+         
         </div>
-      </BrowserRouter>
-    </Provider>
+      </div>
+    </BrowserRouter>
   );
 }
 
